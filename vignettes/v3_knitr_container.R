d33af864980ec7dcf696dc3ec8f4389c37bea0a5
GegznaV <GegznaV@gmail.com>
2016-05-31 13:42:37 +0300

    v0.1.005

--- vignettes/v3_knitr_container.R
+++
@@ -1,6 +0,0 @@
-## ----options1, echo = FALSE, message = FALSE, warning = FALSE------------
-knitr::opts_chunk$set(collapse = TRUE,
-                      comment = "#>",
-                      fig.align = 'center')
-optDEF <- knitr::opts_chunk$get()
-
